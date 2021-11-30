4bd51852c060261da1335eddf17e4eac6b4796d7
Liosion Mikealson <57588337+Liosion@users.noreply.github.com>
2021-11-30 14:02:22 +0330

    Create test.go

---
+++ test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"tyler-smith/go-bip32"
+	"tyler-smith/go-bip39"
+)
+
+func NewKeyManager(bitSize int, mnemonic string) (*KeyManager, error) {
+
+	if mnemonic == "" {
+		entropy, err := bip39.NewEntropy(bitSize)
+		if err != nil {
+			return nil, err
+		}
+		mnemonic, err = bip39.NewMnemonic(entropy)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	km := &KeyManager{
+		mnemonic:   mnemonic,
+		keys:       make(map[string]*bip32.Key, 0),
+	}
+	return km, nil
+}
+
+func (km *KeyManager) GetMnemonic() string {
+	return km.mnemonic
+}
+
+func main() {
+	mnemonic := "abandon ability able about above absent abandon ability able about above absent"
+	km, err := NewKeyManager(128, mnemonic)
+	fmt.Println("Awnser: %s\nError: %s\n", km, err)
+
+
+}
+
+
+
+//"btcsuite/btcd/btcec"
+//"btcsuite/btcd/chaincfg"
+//"btcsuite/btcd/txscript"
+//"btcsuite/btcutil"
